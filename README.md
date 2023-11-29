32a9807e047c8ae1a2d0a52f5fcedd8fd886a08b
acwhelan <acwhelan@usc.edu>
2023-11-29 04:02:53 -0800

    Final Project changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PHYS 408 Final Project
 
-Creating a Pygame of electric field lines animation. 
+2Dand3DElectricFields schowcases monopoles and dipoles in a 2-dimensional and 3-dimensional visualization. I used this Medium Article as a source: https://medium.com/@mathcube7/visualizing-the-electric-field-of-a-dipole-in-python-ca8ddde83c80 
+
+In electricfieldlines.py, I attempt to take the electric_field function I created in the 2-D visualizer and create an ability to add positive and negative charges, and see the resulting fields. 
--- electricfieldlines.py
+++ electricfieldlines.py
@@ -1,99 +1,95 @@
 # Credit to grx6741 github repository here: https://github.com/grx6741/ElectricFieldVisualizing/blob/main/main.py
 
 import pygame
-from numpy import interp
-from math import cos, sin, atan2, pi
+import numpy as np
 
 # Set the dimensions of the window
-width, height = 1280, 720
+width, height = 1000, 800
 window = pygame.display.set_mode((width, height))
 
 # Constant for electric field calculation
-k = 10000
-# Set up the frames per second clock
-fps = pygame.time.Clock()
-FPS = 120
+k = 8.9875517873681764e9
 
-# Function to calculate squared distance between two points (a, b) and (c, d)
-def distSquared(a, b, c, d):
-    return (a-c)**2 + (b-d)**2
-
-# Function to calculate angle between two points (a, b) and (c, d)
-def angleBetween(a, b, c, d):
-    return atan2((d - b), (c - a))
-
-# Class representing marks on the screen
-class Mark:
-    def __init__(self, x, y):
+# Defining our electron class, which takes its own instance as a parameter, a point (x,y), and a charge value 
+class Electron:
+    def __init__(self, x, y, charge):
         self.x = x
         self.y = y
-        self.r = 5           # Initial radius
-        self.stretch = 0     # Initial stretch (for electric field lines)
-        self.angle = 0       # Initial angle
+        self.charge = charge
+        self.dragging = False
 
-    # Function to draw a circle at the mark's position
-    def show_circle(self):
-        pygame.draw.circle(window, (255, 0, 0), (self.x, self.y), self.r)
+    # In this function, the electron is drawn, red if the charge is greater than 0 or blue if it is less than 0 
+    def draw(self):
+        color = (255, 0, 0) if self.charge > 0 else (0, 0, 255)
+        pygame.draw.circle(window, color, (self.x, self.y), 5)
 
-    # Function to draw a line representing the electric field at the mark's position
-    def show_line(self):
-        pygame.draw.line(window, (0, 0, 0), (self.x, self.y), (self.x + self.stretch * cos(self.angle), self.y + self.stretch * sin(self.angle)))
+# Defining our electric field, which takes a point (x,y) and a list of electrons
+def electric_field(x, y, electrons):
+    # Initializes E_x and E_y, and used to accumulate the components of the efield at the point (x, y)
+    E_x, E_y = 0, 0
 
-# Class representing a charge
-class Charge:
-    def __init__(self, charge):
-        self.q = charge      # The magnitude of the charge
-        self.values = []     # Placeholder for future use
+    # Iterating through each electron in the list of electrons 
+    for electron in electrons:
+        # The square of the distance between the point (x,y) and the electron 
+        r_squared = (x - electron.x)**2 + (y - electron.y)**2
+        # Coulomb's law to calculate the magnitude of the efield 
+        magnitude = k * electron.charge / r_squared
+        # The direction of the efield due to each electron 
+        r_hat_x = (x - electron.x) / np.sqrt(r_squared)
+        r_hat_y = (y - electron.y) / np.sqrt(r_squared)
+        # Adding to the Efield components 
+        E_x += magnitude * r_hat_x
+        E_y += magnitude * r_hat_y
+    return E_x, E_y
 
-    # Function to calculate the electric field at a mark's position
-    def electricField1(self, mark):
-        self.pos = pygame.mouse.get_pos()
-        dist = distSquared(self.pos[0], self.pos[1], mark.x, mark.y)
-        if dist != 0:
-            fieldStrength = k * abs(self.q) / dist
-            strength = interp(fieldStrength, [0, 200], [1, 30])
-            mark.r = strength
-            mark.stretch = strength
-            ang = angleBetween(self.pos[0], self.pos[1], mark.x, mark.y)
-            if self.q > 0:
-                mark.angle = ang
-            else:
-                mark.angle = pi + ang
+def draw_electric_field_lines(electrons):
+       for x in range(0, width, 20):
+        for y in range(0, height, 20):
+            E_x, E_y = electric_field(x, y, electrons)
+            length = np.sqrt(E_x**2 + E_y**2)
+            if length > 0:
+                unit_vector_x = E_x / length
+                unit_vector_y = E_y / length
+                pygame.draw.line(window, (0, 0, 0), (x, y), (x + unit_vector_x * 20, y + unit_vector_y * 20), 1)
 
-    # Function to draw electric field lines (currently unused)
-    def drawField(self):
-        for value in self.values:
-            pygame.draw.line(window, (value[0], value[0], value[0]), (value[1], value[2]), (value[1], value[2]))
+def initialize_electric_field_lines(electrons):
+    for x in range(0, width, 20):
+        for y in range(0, height, 20):
+            E_x, E_y = electric_field(x, y, electrons)
+            length = np.sqrt(E_x**2 + E_y**2)
+            if length > 0:
+                unit_vector_x = E_x / length
+                unit_vector_y = E_y / length
+                pygame.draw.line(window, (0, 0, 0), (x, y), (x + unit_vector_x * 20, y + unit_vector_y * 20), 1)
 
-# Number of marks on the screen
-n = 50
-# Create marks on the screen in a grid pattern
-marks = [Mark(width * x // n, height * y // n) for x in range(0, n+1) for y in range(0, n+1)]
+def main():
+    running = True
+    electrons = []
 
-# Create a charge
-electron = Charge(30)
-
-# Function to update the screen every frame
-def update():
-    window.fill((255, 255, 255))
-    for mark in marks:
-        mark.show_line()
-        electron.electricField1(mark)
-    pygame.display.update()
-    fps.tick(FPS)
-
-# Main loop to keep the program running
-def loop():
-    while True:
-        update()
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_ESCAPE]:
-            pygame.quit()
-            quit()
+    while running:
+        window.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                charge = 1 if event.button == 1 else -1
+                electrons.append(Electron(*event.pos, charge))
+            elif event.type == pygame.MOUSEMOTION:
+                for electron in electrons:
+                    if electron.dragging:
+                        electron.x, electron.y = event.pos
+            elif event.type == pygame.MOUSEBUTTONUP:
+                for electron in electrons:
+                    electron.dragging = False
 
-# Start the loop
-loop()
+        draw_electric_field_lines(electrons)
+
+        for electron in electrons:
+            electron.draw()
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
